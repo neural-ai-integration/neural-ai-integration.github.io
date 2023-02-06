a55f64db34ff59d057e26e1c4da73d97fde56de8
Eda AYDIN <edaaydin.ea@outlook.com>
2023-02-07 01:55:02 +0300

    Update 73ad2651535b7ecadd366d5fa045dc32c1cdb8fc38f8243601b62b64f4123420.js

--- js/73ad2651535b7ecadd366d5fa045dc32c1cdb8fc38f8243601b62b64f4123420.js
+++ js/73ad2651535b7ecadd366d5fa045dc32c1cdb8fc38f8243601b62b64f4123420.js
@@ -1,1 +1,14 @@
-!function(){"use strict";const e=Array.from(document.getElementsByTagName("img")),t=e=>{const t=e.parentElement;t&&t.style.removeProperty("animation")};e.forEach((e=>{e.complete?t(e):e.addEventListener("load",(()=>{t(e)}))}))}();
+// @ts-ignore
+! function() {
+    "use strict";
+    const e = Array.from(document.getElementsByTagName("img")),
+        t = e => {
+            const t = e.parentElement;
+            t && t.style.removeProperty("animation")
+        };
+    e.forEach((e => {
+        e.complete ? t(e) : e.addEventListener("load", (() => {
+            t(e)
+        }))
+    }))
+}();
